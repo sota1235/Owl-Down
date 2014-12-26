2a1fae6e0f9fe9c4f0ee0ab58f00fa5ae33e7aea
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-12-26 15:19:34 +0900

    rm jquery slider library

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,8 +2,5 @@ window.BASE_URL = 'http://sota1235.net/owl_js/';
 /* owl_slide.js */
 var s = document.createElement('script');
 s.setAttribute('src', window.BASE_URL + 'owl_slide.js');
-/* jquery slider */
-var j = document.createElement('script');
-j.setAttribute('src', window.BASE_URL + 'jquery.slider.min.js');
 /* set DOMs */
 document.getElementsByTagName('head')[0].appendChild(s).appendChild(j);
