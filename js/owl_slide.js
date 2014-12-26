4f820a0d62faba277fd059a915c03b135e9a6cf1
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-12-26 15:32:54 +0900

    手直し

--- js/owl_slide.js
+++ js/owl_slide.js
@@ -1,51 +1,39 @@
-var pages = []; // スライド情報を突っ込む
-var str   = [];
+(function() {
+  var pages = []; // スライド情報を突っ込む
 
-var page_body = $('.page-body').children();
+  /* pagesにスライド情報をぶっこむ */
+  fetchOwl = function() {
+    var page = [];
+    var page_body = $('.page-body').children();
 
-page_body.each(function() {
-  var t_name   = $(this).prop("tagName");
-  var contents = $(this).get(0).outerHTML;
-  if(t_name == "H1") {
-    if(str.length == 0) {
-      str.push([t_name, contents]);
-    } else {
-      pages.push(str);
-      str = [[t_name, contents]];
-    }
-  } else {
-    if(t_name == "UL") {
-      $(this).children().each(function() {
-        var t_name   = $(this).prop("tagName");
-        var contents = $(this).get(0).outerHTML;
-        str.push([t_name, contents]);
-      });
-    } else {
-      str.push([t_name, contents]);
-    }
-  }
-});
-pages.push(str);
-console.log(pages);
+    page_body.each(function() {
+      var t_name   = $(this).prop("tagName");
+      var contents = $(this).get(0).outerHTML;
+      if(t_name == "H1") {
+        if(page.length == 0) {
+          page.push(contents);
+        } else {
+          pages.push(page);
+          page = [contents];
+        }
+      } else {
+        if(t_name == "UL") {
+          $(this).children().each(function() {
+            var contents = $(this).get(0).outerHTML;
+            page.push(contents);
+          });
+        } else {
+          page.push(contents);
+        }
+      }
+    });
+    pages.push(page);
+  };
 
-/* slide一覧を生成 */
-$('body').append('<div class="slider"></div>');
-var slides = $('.slider');
-
-/* pagesを追加 */
-for(var i=0;i<pages.length;i++) {
-  for(var j=0;j<pages[i].length;j++) {
-    slides.append(pages[i][j][1]);
-  }
-}
-
-/* slider setup */
-$(function() {
-  $('.slider').slider({
-    showControls: true,
-    autoplay: false,
-    showPosition: true;
-    hoverPause: true,
-    direction: 'left'
-  });
+  /* 必要なタグをぶっこむ */
+  attachOwlDown = function() {
+    window.BASE_URL || (window.BASE_URL = 'http://sota1235.net/owlDown/');
+    $('head').append("<link rel='stylesheet' href='" + window.BASE_URL + "owlDown.css' type='text/css'>");
+    $('body').prepend('<div class="owldown"></div>');
+  };
 });
