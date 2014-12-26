cb91c9ebe151571134877d2769b16550af6918f7
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-12-26 15:48:39 +0900

    スライド進めると戻るを追加

--- js/owlDown.js
+++ js/owlDown.js
@@ -1,5 +1,6 @@
 (function() {
-  var pages = []; // スライド情報を突っ込む
+  var pages    = []; // スライド情報を突っ込む
+  var page_num = 0;  // 現在のスライド番号
 
   /* pagesにスライド情報をぶっこむ */
   fetchOwl = function() {
@@ -28,6 +29,9 @@
       }
     });
     pages.push(page);
+    /* titleを追加する */
+    var title = $('title').text();
+    pages.unshift('<h1>' + title + '</h1>');
   };
 
   /* 必要なタグをぶっこむ */
@@ -35,5 +39,17 @@
     window.BASE_URL || (window.BASE_URL = 'http://sota1235.net/owlDown/');
     $('head').append("<link rel='stylesheet' href='" + window.BASE_URL + "owlDown.css' type='text/css'>");
     $('body').prepend('<div class="owldown"></div>');
+  };
+
+  /* スライドを進める */
+  next = function() {
+    if(page_num + 1 == pages.length) return;
+    $('.owldown').html(pages[++page_num]);
+  };
+
+  /* スライドを戻す */
+  prev = function() {
+    if(page_num == 0) return;
+    $('.owldown').html(pages[--page_num]);
   };
 });
