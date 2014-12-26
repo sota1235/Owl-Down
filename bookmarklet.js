b13ab101137e1b7c8bbfae41430fd6c37f29cac5
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-12-26 15:33:26 +0900

    fix URL

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
-window.BASE_URL = 'http://sota1235.net/owl_js/';
+window.BASE_URL = 'http://sota1235.net/owlDown/';
 /* owl_slide.js */
 var s = document.createElement('script');
-s.setAttribute('src', window.BASE_URL + 'owl_slide.js');
+s.setAttribute('src', window.BASE_URL + 'owlDown.js');
 /* set DOMs */
 document.getElementsByTagName('head')[0].appendChild(s).appendChild(j);
