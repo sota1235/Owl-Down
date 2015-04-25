1029849013ff8ae29451d3b5c43311c2dfa899e9
sota1235 <t11460ss@sfc.keio.ac.jp>
2015-04-25 11:42:38 +0900

    add: css追加

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,5 +2,11 @@ window.BASE_URL = 'http://sota1235.net/owl_js/';
 /* owl_slide.js */
 var s = document.createElement('script');
 s.setAttribute('src', window.BASE_URL + 'owl_slide.js');
+/* owl_slide.css */
+var c = document.createElement('link');
+c.setAttribute('rel',  'stylesheet');
+c.setAttribute('type', 'text/css');
+c.setAttribute('href', window.BASE_URL + 'owl_slide.css');
 /* set DOMs */
-document.getElementsByTagName('head')[0].appendChild(s)
+document.getElementsByTagName('head')[0].appendChild(s);
+document.getElementsByTagName('head')[0].appendChild(c);
