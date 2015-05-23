f7e85602100c6b90bf58464ec10f7d605e14c7cc
sota1235 <sota1235@gmail.com>
2015-05-23 17:11:32 +0900

    modify: GitHubのrawデータを読むようにした #5

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,13 @@
-window.BASE_URL = 'http://sota1235.net/owl_js/';
+window.BASE_URL = 'https://cdn.rawgit.com/sota1235/Owl-Down/master/';
 /* owl_slide.js */
 var s = document.createElement('script');
-s.setAttribute('src', window.BASE_URL + 'owl_slide.js');
+s.setAttribute('src', window.BASE_URL + 'js/owl_slide.js');
+s.setAttribute('type', 'text/javascript');
 /* owl_slide.css */
 var c = document.createElement('link');
 c.setAttribute('rel',  'stylesheet');
 c.setAttribute('type', 'text/css');
-c.setAttribute('href', window.BASE_URL + 'owl_slide.css');
+c.setAttribute('href', window.BASE_URL + 'css/owl_slide.css');
 /* set DOMs */
 document.getElementsByTagName('head')[0].appendChild(s);
 document.getElementsByTagName('head')[0].appendChild(c);
